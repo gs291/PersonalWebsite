7b3039c53049cb9393e7e71bab25cd7e7dbb6c00
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-04-06 18:26:46 -0700

    Add starter code (from bloonstl)

---
+++ components/footer/Footer.js
@@ -0,0 +1,48 @@
+import styled from "@emotion/styled";
+import {Typography} from "@material-ui/core";
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
+import siteSizes from "../../lib/utils/siteSizes";
+import {fontFamily} from "../../lib/utils/utils";
+
+const FooterContainer = styled.footer`
+  display: flex;
+  flex-direction: row;
+  height: ${siteSizes.footer.height};
+`;
+
+const FooterText = styled(Typography)`
+  font-family: sans-serif;
+`;
+
+const FooterLink = styled.a`
+  font-family: ${fontFamily};
+  color: #ccc;
+`;
+
+const FooterIcon = styled.div`
+  display: inline-flex;
+  vertical-align: middle;
+`;
+
+export default function Footer({ className }) {
+    return (
+        <FooterContainer className={className}>
+            <FooterText component="span" variant="body2">
+                Written and maintained by
+                &nbsp;
+                <FooterLink href="http://gregsanchez.herokuapp.com/">
+                    Gregory Sanchez
+                </FooterLink>
+                &nbsp;
+                (
+                <FooterLink href="https://www.linkedin.com/in/gregory-sanchez-05b211159">
+                    <FooterIcon>
+                        <LinkedInIcon fontSize="small"/>
+                    </FooterIcon>
+                </FooterLink>
+                )
+            </FooterText>
+        </FooterContainer>
+    );
+}
---
+++ components/page/Page.js
@@ -0,0 +1,66 @@
+import {useEffect} from "react";
+import styled from "@emotion/styled";
+import {useDispatch, useSelector} from "react-redux";
+import { Container, useMediaQuery } from "@material-ui/core";
+
+import Footer from "../footer/Footer";
+import Navbar from "../navbar/Navbar";
+import siteColors from "../../lib/utils/siteColors";
+import {getMobile} from "../../lib/redux/selectors";
+import {updateMobile} from "../../lib/redux/actions";
+
+const PageContainer = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+`;
+
+const Nav = styled(Navbar)`
+  background-image: linear-gradient(${ siteColors.nav.dark }, ${ siteColors.background.main.dark });
+  box-shadow: none;
+`;
+
+const MainContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Foot = styled(Footer)`
+  background-image: linear-gradient(${ siteColors.background.main.dark }, ${ siteColors.nav.dark });
+  justify-content: center;
+  align-items: center;
+  color: ${siteColors.text.dark};
+`;
+
+export default function Page(props) {
+    const dispatch = useDispatch();
+    const mobile = useSelector(getMobile);
+    const screen = useMediaQuery('(max-width: 960px)');
+    useEffect(() => {
+        dispatch(updateMobile(screen));
+    }, [screen]);
+
+    const Main = styled.main`
+      flex: 1;
+      background-color: ${ siteColors.background.main.dark };
+      padding-top: 15px;
+      ${!mobile ? "padding-right: 10px;" : ""}
+      padding-bottom: 30px;
+    `;
+
+    return (
+        <PageContainer>
+            <Nav />
+
+            <Main >
+                <MainContainer maxWidth="lg">
+                    { props.children }
+                </MainContainer>
+            </Main>
+
+            <Foot />
+        </PageContainer>
+    );
+}
