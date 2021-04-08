a1ffa6be26e3ed98f04b8d29dd166d16f67c35c7
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-04-07 23:41:45 -0700

    Update footer to include social media icons

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,6 +1,11 @@
 import styled from "@emotion/styled";
+import {Link} from "@material-ui/core";
 import {useSelector} from "react-redux";
-import {Typography} from "@material-ui/core";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
 
 import siteSizes from "../../lib/utils/siteSizes";
 import siteColors from "../../lib/utils/siteColors";
@@ -11,11 +16,45 @@ const FooterContainer = styled.footer`
   flex-direction: row;
   height: ${siteSizes.footer.height};
   color: ${props => props.dark ? siteColors.text.dark : siteColors.text.light};
-  background-image: linear-gradient(${props => props.dark ? siteColors.background.main.dark : siteColors.background.main.light }, ${props => props.dark ? siteColors.footer.dark :  siteColors.footer.light});
+  background-color: ${props => props.dark ? siteColors.background.main.dark :  siteColors.background.main.light};
 `;
 
-const FooterText = styled(Typography)`
-  font-family: sans-serif;
+const SocialMedia = styled(Link)`
+  width: 50px;
+  height: 50px;
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-bottom: 20px;
+
+  border-radius: 20%;
+  color: ${props => props.dark ? "white" : "black"};
+  transition: 0.3s;
+
+  &:hover {
+    background-color: ${props => props.dark ? "white" : "black"};
+    color: ${props => props.dark ? "black" : "white"};
+  }
+`;
+
+const TW = styled(TwitterIcon)`
+  width: 100%;
+  height: 100%;
+
+`;
+
+const INS = styled(InstagramIcon)`
+  width: 100%;
+  height: 100%;
+`;
+
+const LN = styled(LinkedInIcon)`
+  width: 100%;
+  height: 100%;
+`;
+
+const GH = styled(GitHubIcon)`
+  width: 100%;
+  height: 100%;
 `;
 
 export default function Footer({ className }) {
@@ -23,9 +62,34 @@ export default function Footer({ className }) {
 
     return (
         <FooterContainer className={className} dark={darkMode ? 1 : 0}>
-            <FooterText component="span" variant="body2">
-                Social Media
-            </FooterText>
+            <SocialMedia
+                color="inherit"
+                href="https://twitter.com/GS_291"
+                dark={darkMode ? 1 : 0}
+            >
+                <TW />
+            </SocialMedia>
+            <SocialMedia
+                color="inherit"
+                href="https://www.instagram.com/gregory_sanchez/"
+                dark={darkMode ? 1 : 0}
+            >
+                <INS />
+            </SocialMedia>
+            <SocialMedia
+                color="inherit"
+                href="https://www.linkedin.com/in/gregory-sanchez-05b211159"
+                dark={darkMode ? 1 : 0}
+            >
+                <LN />
+            </SocialMedia>
+            <SocialMedia
+                color="inherit"
+                href="https://github.com/gs291"
+                dark={darkMode ? 1 : 0}
+            >
+                <GH />
+            </SocialMedia>
         </FooterContainer>
     );
 }
