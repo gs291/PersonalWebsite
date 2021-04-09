158bfe4c9e1acddd5c922db395aa00aaf91c75a0
Gregory Sanchez <sanchez.gregory886@gmail.com>
2021-04-08 19:24:40 -0700

    Add dark/light mode colors for mobile navigation bar

--- components/page/Header.js
+++ components/page/Header.js
@@ -1,11 +1,25 @@
 import Head from "next/head";
+import {useSelector} from "react-redux";
+
+import {getDarkMode} from "../../lib/redux/selectors";
+import siteColors from "../../lib/utils/siteColors";
 
 export default function Header() {
+    const darkMode = useSelector(getDarkMode);
+
+    const barColor = darkMode ? siteColors.background.main.dark : siteColors.background.main.light;
     return (
         <>
             <Head>
                 <title>Gregory Sanchez</title>
                 <link rel="icon" href="/favicon.ico" />
+
+                {/*// <!-- Chrome, Firefox OS and Opera -->*/}
+                <meta name="theme-color" content={barColor} />
+                {/*// <!-- Windows Phone -->*/}
+                <meta name="msapplication-navbutton-color" content={barColor} />
+                {/*// <!-- iOS Safari -->*/}
+                <meta name="apple-mobile-web-app-status-bar-style" content={barColor} />
             </Head>
         </>
     );
