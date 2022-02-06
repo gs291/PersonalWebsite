054b9f44477c57a0a7fa5f23f005fa49052ebd8c
Gregory Sanchez <sanchez.gregory886@gmail.com>
2022-02-06 13:56:33 -0800

    Update Project display more information

--- components/project/Project.js
+++ components/project/Project.js
@@ -5,6 +5,9 @@ import ScheduleIcon from '@mui/icons-material/Schedule';
 
 import {getMobile} from '../../lib/redux/selectors';
 import {globalOptions} from '../../lib/utils/emotionStyled';
+import Website from '../icon-links/Website';
+import GitHub from '../icon-links/GitHub';
+import Reddit from '../icon-links/Reddit';
 
 const ProjectContainer = styled('div')`
   display: flex;
@@ -59,6 +62,25 @@ const DetailItem = styled('li')`
   margin-bottom: 7.5px;
 `;
 
+const LinksAndLanguages = styled('div', globalOptions)`
+  display: flex;
+  flex-direction: ${props => props['data-m'] ? 'column' : 'row'};
+  width: 100%;
+  
+  gap: ${props => props['data-m'] ? '20px' : '0'} 0;
+`;
+
+const SpacedFlex = styled('div', globalOptions)`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 0 20px;
+  
+  width: ${props => props['data-m'] ? '100' : props['data-w']}%;
+`;
+
 export default function Project({project}) {
     const mobile = useSelector(getMobile);
 
@@ -86,6 +108,22 @@ export default function Project({project}) {
                         </DetailItem>
                     ))}
                 </ProjectDetailsList>
+                <LinksAndLanguages data-m={mobile}>
+                    <SpacedFlex data-w={30} data-m={mobile}>
+                        {project.locate !== undefined && (<Website href={project.locate} />)}
+                        {project.github !== undefined && (<GitHub href={project.github} />)}
+                        {project.reddit !== undefined && (<Reddit href={project.reddit} />)}
+                    </SpacedFlex>
+                    <SpacedFlex data-w={70} data-m={mobile}>
+                        {project.language.map((language, idx) => (
+                            <div key={idx}>
+                                <Detail variant="body2">
+                                    {language}
+                                </Detail>
+                            </div>
+                        ))}
+                    </SpacedFlex>
+                </LinksAndLanguages>
             </ProjectContainer>
         </>
     );
