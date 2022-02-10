928660f716a482ee3210b994a02e9209cf726230
Gregory Sanchez <sanchez.gregory886@gmail.com>
2022-02-10 00:11:24 -0800

    Update About Me text

--- components/index/AboutMe.js
+++ components/index/AboutMe.js
@@ -73,7 +73,7 @@ export default function AboutMe() {
                 </ImageContainer>
                 <Text variant="body1">
                     I'm a full stack web developer with 7 years of computer science experience!
-                    I've worked and developed in many areas of Computer Science including program, web, android and hardware development.
+                    I've worked and developed in all areas of Computer Science including program, web, android and hardware development.
                 </Text>
                 <Text variant="body1">
                     I was introduced to technology at a very early age.
