5bbb09a17ec07a15ab5c4d550c90fb7a44cebd8c
Gregory Sanchez <sanchez.gregory886@gmail.com>
2022-02-15 20:13:44 -0800

    Add a way to sort projects by langauge

--- components/title/TitleDivider.js
+++ components/title/TitleDivider.js
@@ -10,7 +10,7 @@ import {globalOptions} from '../../lib/utils/emotionStyled';
 const TitleContainer = styled('div')`
   position: sticky;
   top: 0;
-  z-index: 1;
+  z-index: 10;
   margin-top: 30px;
 
   @media only screen and (min-width: 961px) {
